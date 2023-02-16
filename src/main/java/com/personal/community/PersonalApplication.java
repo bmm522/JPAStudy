9e4dc80c615d0eb608fc432b2de8f55c056901d8
bmm522 <rlawldls11@naver.com>
2023-02-16 17:40:19 +0900

    refactor : application name 변경

--- src/main/java/com/personal/community/PersonalApplication.java
+++ src/main/java/com/personal/community/PersonalApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class CallbusLabApplication {
+public class PersonalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CallbusLabApplication.class, args);
+		SpringApplication.run(PersonalApplication.class, args);
 	}
 
 }
--- src/test/java/com/personal/community/PersonalApplicationTests.java
+++ src/test/java/com/personal/community/PersonalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CallbusLabApplicationTests {
+class PersonalApplicationTests {
 
 	@Test
 	void contextLoads() {
